bc7eca21eeeeb2d4b89cc6af9c5fdf10c8b6913a
stanislav_kuznetsov <s.kuznetsov@pravo.ru>
2019-02-26 12:18:25 +0400

    Изменяет имя атрибута

--- src/make-filter.js
+++ src/make-filter.js
@@ -1,9 +1,9 @@
-export default (name, checked = false) => `<input 
+export default (name, isChecked = false) => `<input 
   type="radio" 
   id="filter-${name}" 
   name="filter" 
   value="${name}" 
-  ${checked ? `checked` : ``}>
+  ${isChecked ? `checked` : ``}>
           
   <label class="trip-filter__item" 
   for="filter-${name}">${name}</label>`;
