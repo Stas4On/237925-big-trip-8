a03884993d61dd09dacfb97aa31201de70a961a7
stanislav_kuznetsov <s.kuznetsov@pravo.ru>
2019-06-21 20:04:22 +0400

    Добавляет ServiceWorker

---
+++ public/sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = `BIG_TRIP`;
+
+self.addEventListener(`install`, (evt) => {
+  console.log(`sw, install`, {evt});
+  evt.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll([
+          `./`,
+          `./index.html`,
+          `./trip.html`,
+          `./statistic.html`,
+          `./bundle.js`,
+          `./css/normalize.css`,
+          `./css/main.css`,
+          `./css/flatpickr.min.css`,
+          `./img/star.svg`,
+          `./img/star--check.svg`,
+        ]);
+      }),
+  );
+});
+
+self.addEventListener(`activate`, (evt) => {
+  console.log(`sw`, `activate`, {evt});
+});
+
+self.addEventListener(`fetch`, (evt) => {
+  evt.respondWith(
+    caches.match(evt.request)
+      .then((response) => {
+        console.log(`Find in cache`, {response});
+        return response ? response : fetch(evt.request);
+      })
+      .catch((err) => {
+        console.error({err});
+        throw err;
+      })
+  );
+});
